2b5638b5a05064259bf2340eb6f0a182555b01ed
Matthew Jacobson <matthewjacobson2922@gmail.com>
2023-10-30 10:12:58 -0500

    Initial Commit

---
+++ NotedConversations/NotedConversationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotedConversationsApp.swift
+//  NotedConversations
+//
+//  Created by Matthew Jacobson on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct NotedConversationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
